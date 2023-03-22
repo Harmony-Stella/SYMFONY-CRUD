178d88c1bcbb2aca24acd22fc21bb0b3d5b8ab72
MaFlore <yawo.ahadjitse@ipnetinstitute.com>
2023-03-22 09:57:00 +0000

    update TP2 -V1

--- TP2/src/Controller/TabController.php
+++ TP2/src/Controller/TabController.php
@@ -8,28 +8,22 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TabController extends AbstractController
 {
+   
+    private $tableau;
+
+    public function __construct() {
+        $this->tableau = [
+            1 => ['nom' => 'John', 'age' => 30],
+            2 => ['nom' => 'Jane', 'age' => 25],
+            3 => ['nom' => 'Bob', 'age' => 40],
+        ];
+    }
+
     #[Route('/', name: 'app_tab')]
     public function index(): Response
     {
-        $tableau = [
-            [
-                'id' => 1,
-                'nom' => 'Bic',
-                'couleur' => 'Rouge'
-            ],
-            [
-                'id' => 2,
-                'nom' => 'Ardoise',
-                'couleur' => 'Noire'
-            ],
-            [
-                'id' => 3,
-                'nom' => 'Gourde',
-                'couleur' => 'Bleue'
-            ]
-        ];
         return $this->render('tab/tableau.html.twig', [
-            'tableau' => $tableau,
+            'tableau' => $this->tableau,
         ]);
     }
 
@@ -37,27 +31,18 @@ class TabController extends AbstractController
     public function supprimer(int $id): Response
     {
         // Supprimer l'élément du tableau associatif en fonction de l'ID
-        unset($this->tableau[$id]);
-
-        // Afficher la vue de modification avec l'élément récupéré
-                return $this->render('tab/supprimer.html.twig', [
-                    'id' => $id,
-            ]);
-    
+        unset($this->tableau[$id]);        
         // Rediriger vers la page d'accueil
-        return $this->redirectToRoute('app_tab');
+        return $this->redirectToRoute('app_tab', ['tableau'=>$this->tableau]);
     }
 
     #[Route('/modifier/{id}', name: 'modifier')]
     public function modifier(int $id): Response
     {
         // Récupérer l'élément du tableau associatif en fonction de l'ID
-        $element = $this->tableau[$id];
-
         // Afficher la vue de modification avec l'élément récupéré
         return $this->render('tab/modifier.html.twig', [
-            'id' => $id,
-            'element' => $element
+            
     ]);
 }
 
